3c8e791e57b36dc80d4048f588bbef8f668170c2
thiagomajesk <thiagomgoulart@gmail.com>
2021-04-23 23:55:32 -0300

    Improve docs on smart travel functions

--- src/smart-travel.ts
+++ src/smart-travel.ts
@@ -1,6 +1,11 @@
 import * as vscode from 'vscode'
 import { nextMarkPositionUp, nextMarkPositionDown } from './line-mark'
 
+/**
+ * Travels to the next relevant line above the cursor.
+ * @param textEditor A VS Code TextEditor
+ * @param select If text should be selected when moving or not
+ */
 export function smartTravelUp(textEditor: vscode.TextEditor, select : boolean): void {
   const anchor = textEditor.selection.anchor
 
@@ -12,6 +17,11 @@ export function smartTravelUp(textEditor: vscode.TextEditor, select : boolean): 
   textEditor.revealRange(new vscode.Range(newPosition, newPosition))
 }
 
+/**
+ * Travels to the next relevant line bellow the cursor.
+ * @param textEditor A VS Code TextEditor
+ * @param select If text should be selected when moving or not
+ */
 export function smartTravelDown(textEditor: vscode.TextEditor, select : boolean): void {
   const anchor = textEditor.selection.anchor
 
